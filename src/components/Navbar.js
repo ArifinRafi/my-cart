2442ef8e9375e635ec54595a9d24b01a82698ad5
ArifinRafi <ahmedrafi364@gmail.com>
2023-10-03 19:28:22 +0600

    metataggs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={2} className="menu bg-[#38295B] bg-opacity-90  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-xl w-[120px]">
+      <ul tabIndex={2} className="menu bg-[#38295B] bg-opacity-90 menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-xl w-[120px]">
       <Link to='/' className='btn btn-ghost'>Home</Link>
       <Link to='/About' className='btn btn-ghost'>About</Link>
       <Link to='/products' className='btn btn-ghost'>Products</Link>
