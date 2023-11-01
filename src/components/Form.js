88f7f2e3ec0cb4537630953a421d11366a89868d
ArifinRafi <ahmedrafi364@gmail.com>
2023-11-01 23:32:52 +0600

    mobile res

--- src/components/Form.js
+++ src/components/Form.js
@@ -44,28 +44,28 @@ const Form = () => {
         }
         
         return (
-                <div className='max-w-screen-2xl mx-auto  drop-shadow-lg'>
+                <div className='max-w-screen-2xl  mx-auto  drop-shadow-lg'>
                         <h1 className='text-2xl font-semibold mt-24'>
                         Please take your time and fill the form below. All information you share will be private and only Project Manager can access it.
                         </h1>
-                   <form onSubmit={handleUser} className='my-12 bg-gray-200   h-[800px]'>
+                   <form onSubmit={handleUser} className='my-12 bg-gray-200   h-[900px]'>
                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-items-center '>
-                   <input  type="text" name='name' placeholder="Name" className=" required input input-bordered w-full rounded-md max-w-lg mt-12"
+                   <input  type="text" name='name' placeholder="Name" className=" required input input-bordered lg:w-full w-4/5 rounded-md max-w-lg mt-12"
                     />
-                    <input type="email" name='email' placeholder="Email" className="input input-bordered w-full rounded-md max-w-lg mt-12"
+                    <input type="email" name='email' placeholder="Email" className="input input-bordered lg:w-full w-4/5 rounded-md max-w-lg mt-12"
                     />
-                    <input  type="number" name='phone' placeholder="Phone Number" className="input input-bordered w-full rounded-md max-w-lg mt-12"
+                    <input  type="number" name='phone' placeholder="Phone Number" className="input input-bordered lg:w-full w-4/5 rounded-md max-w-lg mt-12"
                     />
-                    <input type="text" name='address' placeholder="Address" className="input input-bordered w-full rounded-md max-w-lg mt-12"
+                    <input type="text" name='address' placeholder="Address" className="input input-bordered lg:w-full w-4/5 rounded-md max-w-lg mt-12"
                     />
                    </div>
 
                    
 
-                   <div className='grid justify-items-center mt-12'>
-                   <input type="text" name='subject' placeholder="Subject (Project requirements)" className="input input-bordered w-full rounded-md lg:max-w-2lg  "
+                   <div className='grid  justify-items-center mt-12'>
+                   <input type="text" name='subject' placeholder="Subject (Project requirements)" className="input  input-bordered lg:w-full w-4/5 rounded-md lg:max-w-2lg  "
                     /> <br />
-                   <select name='catagory' className="input mb-12 input-bordered w-full  rounded-md max-w-2lg">
+                   <select name='catagory' className="input mb-8 input-bordered lg:w-full w-4/5  rounded-md max-w-2lg">
             <option value="" disabled selected>
               Select Category
             </option>
@@ -73,7 +73,7 @@ const Form = () => {
             <option  value="Category2">Web App</option>
             <option  value="Category3">Mobile App</option>
           </select>
-                    <input type="text" name='description' placeholder="Describe your project in words" className="input input-bordered w-full h-[200px] rounded-md max-w-2lg"
+                    <input type="text" name='description' placeholder="Describe your project in words" className="input input-bordered lg:w-full w-4/5 h-[200px] rounded-md max-w-2lg"
                     />
                     <div className='mt-12'>
                     <ButtonPrimary title='Submit' ></ButtonPrimary>
